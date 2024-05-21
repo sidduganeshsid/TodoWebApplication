e1d5e3ea9c64fb4dfe0799c5701097e8170549ca
sidduganeshsid <sidduganeshengineer@gmail.com>
2024-05-21 14:23:46 +0530

    file renamed

--- TodoWebApp/src/main/java/com/sid/TodoWebApp/TodoWebApplicationMain.java
+++ TodoWebApp/src/main/java/com/sid/TodoWebApp/TodoWebApplicationMain.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodoWebAppApplication {
+public class TodoWebApplicationMain {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TodoWebAppApplication.class, args);
+		SpringApplication.run(TodoWebApplicationMain.class, args);
 	}
 
 }
